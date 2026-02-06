bc145ab7c9acff225852fb5db85b7e765ac52816
Erin Conley <erin.conley@canonical.com>
2026-02-06 09:51:27 -0500

    update sp version and pin to specific dependencies (#60)

--- docs/conf.py
+++ docs/conf.py
@@ -366,10 +366,6 @@ if os.path.exists("./reuse/substitutions.yaml"):
 
 intersphinx_mapping = {
     "juju": ("https://documentation.ubuntu.com/juju/3.6/", None),
-    "starter-pack": (
-        "https://canonical-example-product-documentation.readthedocs-hosted.com/en/latest",
-        None,
-    ),
     "sphinxcontrib-mermaid": (
         "https://sphinxcontrib-mermaid-demo.readthedocs.io/en/latest",
         None,
